575701753d54776ec4940f08f595a67eeb505292
wispo12 <143028172+wispo12@users.noreply.github.com>
2024-04-23 22:35:03 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,12 +1,10 @@
 public class RandomLetterChooser extends RandomStringChooser
 {
 	
-	public RandomLetterChooser(String str)
-	{ 
-		/* to be implemented in part (b) */
-		
-	}
-	
+public RandomLetterChooser(String str)
+{
+    super(getSingleLetters(str));
+}
 	
 	public static String[] getSingleLetters(String str)
 	{ 
